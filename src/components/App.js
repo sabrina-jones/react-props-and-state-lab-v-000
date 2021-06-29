7428b0e84e895057cce521d2c4be561ab3cd8e80
sabrina-jones <sabrina.jones87@gmail.com>
2021-06-29 01:47:24 -0500

    commit tree 6e8a8e07a9a33c0ca620a85b0685b198a20279fd

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,39 @@ class App extends React.Component {
     }
   }
 
+  filterChange = event => {
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        type: event.target.value
+      }
+    })
+  }
+
+  fetchPets = () => {
+    let endpoint = '/api/pets';
+    if (this.state.filters.type != 'all'){
+      endpoint += `?type=${this.state.filters.type}`;
+    }
+
+    fetch(endpoint)
+      .then(res => res.json())
+      .then(data => this.setState({pets: data}))
+  }
+
+  handleAdopt = (id) => {
+    const pets = this.state.pets.map(pet => {
+       if (pet.id === id) {
+         return {...pet, isAdopted: true}
+       } else {
+         return pet 
+       }
+     })
+     this.setState({
+       pets: pets
+     })
+   }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +57,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.filterChange} onFindPetsClick={this.fetchPets}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser  pets={this.state.pets} onAdoptPet= {this.handleAdopt} />
             </div>
           </div>
         </div>
